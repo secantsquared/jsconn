33dd315667269c9d386b55f47118793f2986c896
ryanboris <ryan.boris@gmail.com>
2019-03-14 23:58:58 -0400

    deps

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const express = require('express')
 const cors = require('cors')
 const server = express()
+const psTree = require('ps-tree')
 
 server.use(cors())
 
+var kill = function(pid, signal, callback) {
+  signal = signal || 'SIGKILL'
+  callback = callback || function() {}
+  var killTree = true
+  if (killTree) {
+    psTree(pid, function(err, children) {
+      ;[pid]
+        .concat(
+          children.map(function(p) {
+            return p.PID
+          })
+        )
+        .forEach(function(tpid) {
+          try {
+            process.kill(tpid, signal)
+          } catch (ex) {}
+        })
+      callback()
+    })
+  } else {
+    try {
+      process.kill(pid, signal)
+    } catch (ex) {}
+    callback()
+  }
+}
 
+// ... somewhere in the code of Yez!
 server.get('/', async (req, res, next) => {
   if (!req.query.username) {
-    res.status(404).json({message: "BAD REQUEST"})
+    res.status(404).json({ message: 'BAD REQUEST' })
   }
-  var usernameRegex = /^[a-zA-Z0-9]+$/
-  if (!usernameRegex.test(req.query.username)) {
-    res.status(400).json({message: "bad request"})
-  }
+
   const py = await require('child_process').spawn('python', [
     './app.py',
     req.query.username
   ])
+
   try {
     py.stdout.on('data', data => {
       res.json(JSON.parse(data))
@@ -31,6 +57,7 @@ server.get('/', async (req, res, next) => {
     )
   } finally {
     py.on('close', (code, signal) => {
+      kill(py.pid)
       console.log(`child process exited with code ${code} and signal ${signal}`)
     })
   }
